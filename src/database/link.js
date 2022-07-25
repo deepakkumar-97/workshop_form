2632d3e6874e18f65c90134ea13e807e7eb44ade
“Deepak <deepakkumar.4648@gmail.com>
2022-07-25 23:22:37 +0530

    link update

--- src/database/link.js
+++ src/database/link.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/registration_form")
-.then(() => {
+// code for connection with with local database
+// mongoose.connect("mongodb://localhost:27017/registration_form")
+
+mongoose.connect("mongodb+srv://Deepak:9999mongo@cluster0.v8gzl.mongodb.net/registration_form?retryWrites=true&w=majority").then(() => {
     console.log(`Connection Done`)
 }).catch((e) => {
-    console.log(`No connection`)
+    console.log(e)
 })
